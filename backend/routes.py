488d25f369eec72209d4352d08bc9fb4be052ebe
Lucas Rocha <lr359508@gmail.com>
2024-05-08 11:28:00 -0300

    feat: schema changed

--- backend/routes.py
+++ backend/routes.py
@@ -1,24 +1,20 @@
 
-from flask import Blueprint
+from flask import Blueprint, request
 
 bp = Blueprint("Images", "Images", __name__, url_prefix="/image")
 
-@bp.route('/', methods=['GET'])
-def getImages():
-    return 'Get all images'
+@bp.route('/', methods=['GET', 'POST'])
+def setImages():
+    if request.method == 'GET':
+        return 'Get all images'
+    else:
+        return 'Create new image'
 
-@bp.route('/', methods=['POST'])
-def postImage():
-    return 'Create new image'
-
-@bp.route('/id', methods=['GET'])
-def getImage():
-    return 'Get image info'
-
-@bp.route('/id', methods=['PUT'])
-def updateImage():
-    return 'Update image info'
-
-@bp.route('/id', methods=['DELETE'])
-def deleteImage():
-    return 'Delete image info'
+@bp.route('/id', methods=['GET', 'PUT', 'DELETE'])
+def setImageDetail():
+    if request.method == 'GET':
+        return 'Get image info'
+    elif request.method == 'PUT':
+        return 'Update image info'
+    else:
+        return 'Delete image info'
