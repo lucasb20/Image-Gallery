57e8592d815bc6bf452d21b9db4c63d43eb26cbc
Lucas Rocha <lr359508@gmail.com>
2024-05-08 12:06:42 -0300

    feat: MAX_CONTENT_LENGTH added

--- backend/app.py
+++ backend/app.py
@@ -7,5 +7,6 @@ UPLOAD_FOLDER = '/uploads'
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY") or 'dev'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
 app.register_blueprint(bp)
