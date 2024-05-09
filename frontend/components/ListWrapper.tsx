be66e110fbde5027eb7cb98bed1d20982389c136
Lucas Rocha <lr359508@gmail.com>
2024-05-09 16:36:27 -0300

    feat: image-wrapper created

--- frontend/components/ListWrapper.tsx
+++ frontend/components/ListWrapper.tsx
@@ -1,13 +1,25 @@
 "use client"
 
+import { getImageList } from "@/services/APIService"
+import { ImageInfo } from "@/services/Interfaces"
+import Image from "next/image"
 import { useEffect, useState } from "react"
 
 export function ListWrapper(){
-    const [todos, setTodos] = useState([])
+    const [imgs, setImgs] = useState<ImageInfo[]>([])
+
+    useEffect(() => {
+        getImageList()
+        .then(data => {
+            setImgs(data)
+        })
+    }, [])
 
     return(
         <div className="image-wrapper">
-            list of images
+            {imgs.map((img, index) => {
+                return <Image src={`${img.id}.${img.extension}`} width={200} height={200} alt={img.title} key={img.id}/>
+            })}
         </div>
     )
 }
--- frontend/services/APIService.tsx
+++ frontend/services/APIService.tsx
@@ -3,6 +3,6 @@ import { ImageInfo } from "@/services/Interfaces"
 const API_URL = 'http://localhost:5000'
 
 export async function getImageList(){
-    const response = await fetch(`${API_URL}/todo`)
+    const response = await fetch(`${API_URL}/images/`)
     return response.json()
 }
