b12a473bbb7ace40bd906ddd728c5bd1c0aa9ac4
Lucas Rocha <lr359508@gmail.com>
2024-05-09 11:24:51 -0300

    fix: max size to 256 chars

--- backend/schemas.py
+++ backend/schemas.py
@@ -2,8 +2,8 @@ from marshmallow import Schema, fields, validate
 
 class ImageSchema(Schema):
     id = fields.Integer(dump_only=True)
-    title = fields.String(required=True, validate=validate.Length(max=255))
+    title = fields.String(required=True, validate=validate.Length(max=256))
     description = fields.String(allow_none=True)
-    author = fields.String(required=True, validate=validate.Length(max=255))
-    signature = fields.String(required=True, validate=validate.Length(max=255))
+    author = fields.String(required=True, validate=validate.Length(max=256))
+    signature = fields.String(required=True, validate=validate.Length(max=256))
     created = fields.DateTime(dump_only=True)
