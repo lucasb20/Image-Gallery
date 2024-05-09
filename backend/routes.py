f3160022c365efc5211a836b814917c65e200915
Lucas Rocha <lr359508@gmail.com>
2024-05-09 13:03:26 -0300

    feat: image query created

--- backend/routes.py
+++ backend/routes.py
@@ -1,6 +1,8 @@
 
 from flask import Blueprint, request, jsonify, current_app, redirect, url_for, send_from_directory
 from marshmallow.exceptions import ValidationError
+from sqlalchemy import select
+from backend.database import db
 from backend.models import Image
 from backend.utils import allowed_file, InsertImage
 from backend.schemas import ImageSchema
@@ -11,7 +13,16 @@ bp = Blueprint("Images", __name__, url_prefix="/images")
 @bp.route('/', methods=['GET', 'POST'])
 def setImages():
     if request.method == 'GET':
-        return ImageSchema(many=True).dumps(Image.query.all())
+        args = request.args.to_dict()
+        query = select(Image)
+        if 'id' in args:
+            query = query.where(Image.id == int(args['id']))
+        if 'author' in args:
+            query = query.where(Image.author == args['author'])
+        if 'signature' in args:
+            query = query.where(Image.signature == args['signature'])
+        res = db.session.execute(query).all()
+        return ImageSchema(many=True).dumps(res)
     else:
         try:
             cont = ImageSchema().load(request.args.to_dict())
